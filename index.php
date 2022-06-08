20926fe023347b053a24aa6933f58bf7a58845e8
davide1217 <davidepizzola17@gmail.com>
2022-06-08 17:47:12 +0200

    completo-no-footer

--- index.php
+++ index.php
@@ -76,10 +76,32 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="style.css">
   <title>Document</title>
 </head>
 <body>
+  <header>
+    <div>
+      <div>
+        <img src="https://www.pngplay.com/wp-content/uploads/13/Google-Logo-Transparent-Free-PNG.png" alt="google">
+        <span>Privacy e termini</span>
+      </div>
+
+      <div>
+        <i class="fa-solid fa-grip-vertical"></i>
+        <div id="profileImg"></div>
+      </div>
+    </div>
+
+    <nav>
+      <a href="#">Introduzione</a>
+      <a href="#">Norme sulla privacy</a>
+      <a href="#">Termini di servizio</a>
+      <a href="#">Tecnologie</a>
+      <a class="active" href="#">Domande frequenti</a>
+    </nav>
+  </header>
   <main>
     <div class="container">
       <?php foreach ($faqs as $faq):?>
