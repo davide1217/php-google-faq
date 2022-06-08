42fcc61e095b2aa0d798941eb3dbaaac0740f317
davide1217 <davidepizzola17@gmail.com>
2022-06-08 18:34:42 +0200

    completo-perfezionato

--- index.php
+++ index.php
@@ -33,14 +33,14 @@
       <ol>
         <li>
           La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla   privacy   vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
-            <ol>
+            <ul>
               <li>
               Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a   Islanda,  Liechtenstein e Norvegia) o in Svizzera.
               </li>
               <li>
                 Google LLC, con sede negli Stati Uniti, per il resto del mondo.
               </li>
-            </ol>
+            </ul>
         </li>
         <li>
           La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
@@ -110,5 +110,22 @@
       <?php endforeach ?>
     </div>
   </main>
+  <footer>
+    <div class="container">
+      <nav>
+        <a href="#"> Google </a> <i class="fa-solid fa-circle"></i>
+        <a href="#"> Tutto su Google </a> <i class="fa-solid fa-circle"></i>
+        <a href="#"> Privacy </a> <i class="fa-solid fa-circle"></i>
+        <a href="#"> Termini</a>
+      </nav>
+
+      <div>
+        <i class="fa-solid fa-globe"></i>
+        <select name="" id="">
+          <option value="it">italiano</option>
+        </select>
+      </div>
+    </div>
+  </footer>
 </body>
 </html>
